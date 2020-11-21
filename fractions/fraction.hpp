f85904771ea0df973309ae97b3ba480cdb73f7b1
Dhauhner Student <dhauhner@gmail.com>
2020-11-21 15:39:09 +0100

    add parsing values in main

--- fractions/fraction.hpp
+++ fractions/fraction.hpp
@@ -1,5 +1,5 @@
 #pragma once
-//this include is needed because of defined operatator<< (std::ostream)
+// this include is needed because of defined operatator<< (std::ostream)
 #include <iostream>
 
 namespace Numbers {
@@ -9,28 +9,28 @@ public:
     Fraction();
     Fraction(long long number);
     Fraction(long long numerator, long long denumerator);
-    //copy constructor
+    // copy constructor
     Fraction(const Fraction& fraction);
-    //destructor
+    // destructor
     ~Fraction() = default;
-    //define own operators for basic math operations;
+    // define own operators for basic math operations;
     // as shown: return value of this operation is just another
     // Fraction (result of operation)
     Fraction abs() const;
 
-    Fraction operator* (const Fraction& another) const;
-    Fraction operator/ (const Fraction& another) const;
-    Fraction operator+ (const Fraction& another) const;
-    Fraction operator- (const Fraction& another) const;
-    //define own operators for value comparison.
-    bool operator!= (const Fraction& another) const;
-    bool operator== (const Fraction& another) const;
-    bool operator< (const Fraction& another) const;
-    bool operator> (const Fraction& another) const;
-    //friend operator<< for proper printing of fraction when using output stream.
+    Fraction operator*(const Fraction& another) const;
+    Fraction operator/(const Fraction& another) const;
+    Fraction operator+(const Fraction& another) const;
+    Fraction operator-(const Fraction& another) const;
+    // define own operators for value comparison.
+    bool operator!=(const Fraction& another) const;
+    bool operator==(const Fraction& another) const;
+    bool operator<(const Fraction& another) const;
+    bool operator>(const Fraction& another) const;
+    // friend operator<< for proper printing of fraction when using output stream.
     friend std::ostream& operator<<(std::ostream& os, const Fraction& fraction);
 private:
     long long numerator;
     long long denumerator;
 };
-} //namespace Numbers
+} // namespace Numbers
--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include "fractions/fraction.hpp"
 #include "determinant_calculation/calc_det.hpp"
 
+#include <algorithm>
 #include <iostream>
 #include <limits>
 #include <string>
 #include <sstream>
 #include <utility>
 #include <vector>
+#include <cctype>
 
-// TODO ===================================================================
-// add function for passing fractions (somekind of parser)
-// add function for passing whole matrix...
-// and add switch statement - a in main function, we can have some options...
 using namespace Numbers;
 
 // print matrix by iterating through whole matrix, and printing element
@@ -19,17 +17,59 @@ using namespace Numbers;
 // to implement an overload of operator<<.
 void print_matrix(const Matrix& matrix)
 {
-    // for-each loop: thanks to auto (std 11) i do not have to use
-    // whole type of reference on loop ( which is std::vector<Fraction>
-    // for row, and Fraction for element )
-    // https://en.cppreference.com/w/cpp/language/auto
-    std::cout << "Matrix:" << std::endl;
+    std::cout <<
+        std::endl <<
+        "User provided matrix:" <<
+        std::endl;
+    // standard for-each loop.
+    // for each row in matrix (std::vector<Fraction>)
     for ( auto& row: matrix ) {
+        // standard for-each loop
+        // for each element in row (Fraction)
         for ( auto& element: row ) {
             std::cout << element << " ";
         }
         std::cout << std::endl;
     }
+}
+// function which parse string provided by user: it checks 
+// if size is proper, and  
+std::vector<Fraction> parse_row(std::string& row, int size) 
+{
+    std::istringstream iss(row);
+    std::string str;
+    std::vector<Fraction> row_vector;
+    while (iss >> str) {
+        // at first, we assume, that provided value is positive.
+        bool is_positive = true;
+        // check if value is negative;
+        if (*str.begin() == '-') {
+            // iterator to first char in char array under string.
+            auto first = str.begin();
+            // first in algorithm below must be digit - if negative value
+            // is provided than we should start from second number.
+            first++;
+            // if '-' was the only char in string - that we should throw
+            // an error.
+            str = std::string(first, str.end());
+            is_positive = false;
+        }
+        auto is_digit = [](unsigned char c){ return std::isdigit(c); };
+        //here is algorithm, which is checking if all values are digits.
+        if (!std::all_of(str.begin(), str.end(), is_digit)) {
+            throw std::runtime_error("Incorrect value in row provided!");
+        } else {
+            auto number = std::stoi(str);
+            number = is_positive? number : -number;
+            row_vector.push_back(Fraction(number));
+        }
+    }
+    // for square matrixes size of a row must be equal to number of rows (7x7 by example.)
+    // also - all rows in any matrix must be same size.
+    if (row_vector.size() != size) {
+        throw std::runtime_error("Incorect number of elements!");
+    }
+    return row_vector;
 }
 
 Matrix parse_matrix(int size)
@@ -37,37 +77,39 @@ Matrix parse_matrix(int size)
     Matrix matrix;
     //standard for loop, with counter i;
     for (int i = 0; i < size; i++) {
-        matrix.push_back(std::vector<Fraction>());
         std::string input;
         std::cout << "row " << i+1 << ":";
+        // here we are trying to get proper value of row in matrix from user.
+        // this is: space separated integer values.
         std::getline(std::cin, input);
-        std::cin.clear();
-        std::istringstream iss(input);
-        int number;
-
-        while (iss >> number) {
-            if (iss.fail()) {
-                std::cerr << "Incorrect value in row provided! Please pass proper row (INTEGERS ONLY)!";
-                matrix.pop_back();
-                i--;
-            } else {
-            matrix[i].push_back(Fraction(number));
-            }
+        try {
+            // if parse_row does not throw error - than push back new row to matrix.
+            matrix.push_back(parse_row(input, size));
+        } catch (std::runtime_error& e) {
+            // if runtime error was thrown, write to console message of this error.
+            std::cout <<
+                e.what() <<
+                std::endl;
+            // decrement i value, for user to pass proper value of matrix row.
+            --i;
         }
     }
     return matrix;
 }
 
-int main(int argc, char** argv)
+int run_program()
 {
     // Determinant can only be calculated from square matrixes;
     // program will exit with warning message
     std::cout << "What is size of matrix?" << std::endl;
     int a;
     std::cin >> a;
-    while(!std::cin.good() || a <= 0){
-        std::cerr << "Incorrect value provided! Value must be positive integer!"
-            << "Enter positive integer ";
+    while (!std::cin.good() || a <= 0) {
+        std::cout <<
+            "Incorrect value provided! Value must be positive integer!" <<
+            std::endl <<
+            "Enter positive integer:" <<
+            std::endl;
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cin >> a;
@@ -76,13 +118,26 @@ int main(int argc, char** argv)
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     auto matrix = parse_matrix(a);
     print_matrix(matrix);
-
+    Fraction determinant;
     try {
-        std::cout << "determinant value is: " << calculate_determinant(matrix) << std::endl;
-    } catch (std::runtime_error& e) {
+        determinant = calculate_determinant(matrix);
+    } catch ( std::runtime_error& e ) {
         std::cerr << "program meet runtime problem: " << e.what() << std::endl;
+        // if return value for main is different than 0, it means, that there was problem.
+        // and program does not succesfully finish job.
         return 1;
     }
+    // print determinant value.
+    std::cout <<
+        std::endl <<
+        "determinant of above matrix is equal: " <<
+        determinant <<
+        std::endl;
     // return 0 in main means, that program has succesfully finish job.
     return 0;
+}
+
+int main(int argc, char** argv)
+{
+    return run_program();
 }
